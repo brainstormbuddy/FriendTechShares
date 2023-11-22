37f325ecbf12de7eab5eed3ec5160a53877ae825
Adam Sims <adamsims1203@gmail.com>
2023-11-22 18:19:46 +0200

    feat: update a deploy script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,44 @@
 require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@openzeppelin/hardhat-upgrades');
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+      {
+        version: "0.8.20",
+        settings: {},
+      },
+    ],
+  },
+  settings: {
+    optimizer: {
+      enabled: true,
+      runs: 1000,
+    },
+  },
+  networks: {
+    // for mainnet
+    'base-mainnet': {
+      url: 'https://mainnet.base.org',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+    // for testnet
+    'base-goerli': {
+      url: 'https://goerli.base.org/',
+      accounts: [process.env.WALLET_KEY],
+      gasPrice: 1000000000,
+    },
+  },
+  etherscan: {
+    apiKey: "N1BM63Y2TUKTX8ZW9MXKCNQ8BPZH73MKIY"
+  },
+  sourcify: {
+    enabled: true
+  }
 };
