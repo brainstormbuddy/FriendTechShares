2d4d46d417ee9656bf5c5c79b06ee9b294de5893
Adam Sims <adamsims1203@gmail.com>
2023-11-22 18:18:48 +0200

    feat: update a contract as upgradeable

---
+++ contracts/FriendTechShares.sol
@@ -0,0 +1,174 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.19;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+
+// TODO: Events, final pricing model,
+
+contract FriendtechSharesV1 is OwnableUpgradeable {
+    address public protocolFeeDestination;
+    uint256 public protocolFeePercent;
+    uint256 public subjectFeePercent;
+    uint256 public initialPrice;
+
+    event Trade(
+        address trader,
+        address subject,
+        bool isBuy,
+        uint256 shareAmount,
+        uint256 ethAmount,
+        uint256 protocolEthAmount,
+        uint256 subjectEthAmount,
+        uint256 supply
+    );
+
+    // SharesSubject => (Holder => Balance)
+    mapping(address => mapping(address => uint256)) public sharesBalance;
+
+    // SharesSubject => Supply
+    mapping(address => uint256) public sharesSupply;
+
+    // SharesSubject => Way
+    mapping(address => uint256) public sharesWay;
+
+    function initialize(uint256 _price) public {
+        initialPrice = _price;
+    }
+
+    function setFeeDestination(address _feeDestination) public onlyOwner {
+        protocolFeeDestination = _feeDestination;
+    }
+
+    function setProtocolFeePercent(uint256 _feePercent) public onlyOwner {
+        protocolFeePercent = _feePercent;
+    }
+
+    function setSubjectFeePercent(uint256 _feePercent) public onlyOwner {
+        subjectFeePercent = _feePercent;
+    }
+
+    function setSubjectWay(
+        address _sharesSubject,
+        uint256 _way
+    ) public onlyOwner {
+        require(_way >= 0 && _way <= 2, "Invalid value for sharesWay");
+        sharesWay[_sharesSubject] = _way;
+    }
+
+    function getPrice(
+        uint256 supply,
+        uint256 amount,
+        address sharesSubject
+    ) public view returns (uint256) {
+        if (sharesWay[sharesSubject] == 0) {
+            return (amount * 2 * 1 ether) / 8000;
+        } else if (sharesWay[sharesSubject] == 1) {
+            return ((2 * (supply - 1 + amount)) * 1 ether) / 12000;
+        } else {
+            return (2 ** (amount - 1) * 1 ether) / 16000;
+        }
+    }
+
+    function getBuyPrice(
+        address sharesSubject,
+        uint256 amount
+    ) public view returns (uint256) {
+        return getPrice(sharesSupply[sharesSubject], amount, sharesSubject);
+    }
+
+    function getSellPrice(
+        address sharesSubject,
+        uint256 amount
+    ) public view returns (uint256) {
+        return
+            getPrice(
+                sharesSupply[sharesSubject] - amount,
+                amount,
+                sharesSubject
+            );
+    }
+
+    function getBuyPriceAfterFee(
+        address sharesSubject,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 price = getBuyPrice(sharesSubject, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        return price + protocolFee + subjectFee;
+    }
+
+    function getSellPriceAfterFee(
+        address sharesSubject,
+        uint256 amount
+    ) public view returns (uint256) {
+        uint256 price = getSellPrice(sharesSubject, amount);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        return price - protocolFee - subjectFee;
+    }
+
+    function buyShares(address sharesSubject, uint256 amount) public payable {
+        uint256 supply = sharesSupply[sharesSubject];
+        require(
+            supply > 0 || sharesSubject == msg.sender,
+            "Only the shares' subject can buy the first share"
+        );
+        uint256 price = getPrice(supply, amount, sharesSubject);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        require(
+            msg.value >= price + protocolFee + subjectFee,
+            "Insufficient payment"
+        );
+        sharesBalance[sharesSubject][msg.sender] =
+            sharesBalance[sharesSubject][msg.sender] +
+            amount;
+        sharesSupply[sharesSubject] = supply + amount;
+        emit Trade(
+            msg.sender,
+            sharesSubject,
+            true,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            supply + amount
+        );
+        (bool success1, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success2, ) = sharesSubject.call{value: subjectFee}("");
+        require(success1 && success2, "Unable to send funds");
+    }
+
+    function sellShares(address sharesSubject, uint256 amount) public payable {
+        uint256 supply = sharesSupply[sharesSubject];
+        require(supply > amount, "Cannot sell the last share");
+        uint256 price = getPrice(supply - amount, amount, sharesSubject);
+        uint256 protocolFee = (price * protocolFeePercent) / 1 ether;
+        uint256 subjectFee = (price * subjectFeePercent) / 1 ether;
+        require(
+            sharesBalance[sharesSubject][msg.sender] >= amount,
+            "Insufficient shares"
+        );
+        sharesBalance[sharesSubject][msg.sender] =
+            sharesBalance[sharesSubject][msg.sender] -
+            amount;
+        sharesSupply[sharesSubject] = supply - amount;
+        emit Trade(
+            msg.sender,
+            sharesSubject,
+            false,
+            amount,
+            price,
+            protocolFee,
+            subjectFee,
+            supply - amount
+        );
+        (bool success1, ) = msg.sender.call{
+            value: price - protocolFee - subjectFee
+        }("");
+        (bool success2, ) = protocolFeeDestination.call{value: protocolFee}("");
+        (bool success3, ) = sharesSubject.call{value: subjectFee}("");
+        require(success1 && success2 && success3, "Unable to send funds");
+    }
+}
